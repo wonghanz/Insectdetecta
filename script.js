b08f4afc905ec94641a8ce3f86620b32712fd040
wonghanz <115807670+wonghanz@users.noreply.github.com>
2023-06-17 20:36:02 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,17 +4,36 @@ async function loadModel() {
   return model;
 }
 
-
-
-// Run predictions on input data
+// Run predictions on input image
 async function predict() {
   const model = await loadModel();
-  const input = // your input data (e.g., image, video frame, etc.)
-  const predictions = await model.predict(input);
-  // Process the predictions
-  console.log(predictions);
+  const inputElement = document.getElementById('input-image');
+  const image = await loadImage(inputElement);
+  const processedImage = preprocessImage(image);
+  const prediction = await model.predict(processedImage);
+  // Process the prediction and update the website accordingly
+  console.log(prediction);
 }
 
-// Call the predict() function to make predictions
-predict();
+// Load image from input file
+function loadImage(inputElement) {
+  return new Promise((resolve, reject) => {
+    const file = inputElement.files[0];
+    const reader = new FileReader();
+    reader.onload = () => {
+      const image = new Image();
+      image.onload = () => resolve(image);
+      image.onerror = reject;
+      image.src = reader.result;
+    };
+    reader.onerror = reject;
+    reader.readAsDataURL(file);
+  });
+}
 
+// Preprocess the input image
+function preprocessImage(image) {
+  const tensor = tf.browser.fromPixels(image).resize([YOUR_IMAGE_WIDTH, YOUR_IMAGE_HEIGHT]);
+  // Preprocessing steps specific to your model, such as normalization, resizing, etc.
+  return tensor;
+}
