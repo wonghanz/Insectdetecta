6bb6daebafa423321719b9a32005b531a14bda4f
wonghanz <115807670+wonghanz@users.noreply.github.com>
2023-06-17 19:04:18 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+// Load the model
+async function loadModel() {
+  const model = await tf.loadLayersModel('path/to/model.json');
+  return model;
+}
+
+// Run predictions on input data
+async function predict() {
+  const model = await loadModel();
+  const input = // your input data (e.g., image, video frame, etc.)
+  const predictions = await model.predict(input);
+  // Process the predictions
+  console.log(predictions);
+}
+
+// Call the predict() function to make predictions
+predict();
 
